8eb301c96c42078b6d3c7d96525969c410782f4b
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-09-12 14:16:18 +0900

    py3.4 comatibility

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ classifiers = [
     'License :: OSI Approved :: Apache Software License',
     'Natural Language :: English',
     'Operating System :: POSIX',
+    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3 :: Only'
