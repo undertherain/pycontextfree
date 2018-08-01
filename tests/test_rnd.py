f9b11797b0870d588da7d24b83b791c5faa1146e
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-08-01 13:40:32 +0900

    test random

---
+++ tests/test_rnd.py
@@ -0,0 +1,17 @@
+import unittest
+from contextfree.contextfree import circle
+from contextfree.contextfree import init
+from contextfree.contextfree import rnd, prnd, coinflip
+
+
+class Tests(unittest.TestCase):
+
+    def test_rnd(self):
+        init(face_color="#123456", background_color="#aaaaaa")
+        circle(1)
+        res = rnd(1)
+        self.assertLess(res, 1.01)
+        res = prnd(1)
+        self.assertLess(res, 1.01)
+        res = coinflip(2)
+        self.assertIn(res, [True, False])
