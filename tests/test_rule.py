4612222c03a1ea159b84dca13c0e1796b63c8d3c
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-09-12 14:05:25 +0900

    not implemented if parameters to rule

--- tests/test_rule.py
+++ tests/test_rule.py
@@ -1,21 +1,31 @@
+"""Unittests for rule feature"""
+
+import logging
 import unittest
 from contextfree.contextfree import init, register_rule, call_rule
 
 
-@register_rule("wall", 1)
-def rule():
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+
+@register_rule(1)
+def wall():
+    """example of one instance of a rule"""
     print("I am a rule 1")
 
 
-@register_rule("wall", 1)
-def rule():
+@register_rule(1)
+def wall():
+    """example of another instance of a rule"""
     print("I am a rule 2")
 
 
 class Tests(unittest.TestCase):
-
-    def test_rnd(self):
+    """The actual unittest class"""
+    def test_rule(self):
+        """envoking the rule"""
         init(face_color="#123456", background_color="#aaaaaa")
         call_rule("wall")
-        # with self.assertRaises(RuntimeError):
-        rule()
+        wall()
+        with self.assertRaises(NotImplementedError):
+            wall("wrong")
