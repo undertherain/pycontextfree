b227c897ed7b8cd6c8b083c2e183fb7d6a0ddf1f
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-09-12 14:14:57 +0900

    fix map example to match new rule syntax

--- examples/Ancient Map/map.py
+++ examples/Ancient Map/map.py
@@ -6,16 +6,18 @@ from contextfree.contextfree import *
 # logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 
-@register_rule("wall", 1)
-def wall_1():
+# pylint: disable=E0102
+@register_rule(1)
+def wall():
     with translate(0.95, 0):
         with rotate(0.01):
             with scale(0.975):
                 ancient_map()
 
 
-@register_rule("wall", 1)
-def wall_2():
+# pylint: disable=E0102
+@register_rule(1)
+def wall():
     box()
     with translate(0.95, 0):
         with rotate(-0.0027):
@@ -24,8 +26,9 @@ def wall_2():
                     ancient_map()
 
 
-@register_rule("wall", 0.09)
-def wall_4():
+# pylint: disable=E0102
+@register_rule(0.09)
+def wall():
     box()
     with scale(0.975):
         with translate(0.95, 0):
@@ -35,8 +38,9 @@ def wall_4():
                 ancient_map()
 
 
-@register_rule("wall", 0.05)
-def wall_5():
+# pylint: disable=E0102
+@register_rule(0.05)
+def wall():
     with translate(0.97, 0):
         with scale(1.5):
             with rotate(math.pi / 2):
@@ -47,13 +51,7 @@ def wall_5():
 
 @check_limits
 def ancient_map():
-    call_rule("wall")
-    #box()
-    #with translate(1, 0):
-     #   with scale(0.97):
-      #      with rotate(0.01):
-       #         with color(hue=0.001, saturation=0.1, lightness=0.003):
-        #            ancient_map()
+    wall()
 
 
 def main():
--- tests/test_rule.py
+++ tests/test_rule.py
@@ -8,18 +8,21 @@ from contextfree.contextfree import init, register_rule, call_rule
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 
+# pylint: disable=E0102
 @register_rule(1)
 def wall():
     """example of one instance of a rule"""
     print("I am a rule 1")
 
 
+# pylint: disable=E0102
 @register_rule(1)
 def wall():
     """example of another instance of a rule"""
     print("I am a rule 2")
 
 
+# pylint: disable=E1121
 class Tests(unittest.TestCase):
     """The actual unittest class"""
     def test_rule(self):
